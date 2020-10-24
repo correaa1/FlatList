2dce718a0f6d8ad55a1dc4290dc43209b52f39c3
correaa1 <bruno_correa_ds@hotmail.com>
2020-10-24 19:50:00 -0300

    Finalizado

--- App.js
+++ App.js
@@ -1,60 +1,55 @@
-import React, { Component } from 'react';
-import { View, 
-  Text,
-  StyleSheet,
-  FlatList
- } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      feed:[
-        {nome: 'Bruno', idade: 23, email:'bruno@gmail.com'},
-        {nome: 'Pablo', idade: 24, email:'pablo@gmail.com'},
-        {nome: 'Fulano', idade: 39, email:'fulano@gmail.com'},
-        {nome: 'Ciclano', idade: 18, email:'ciclano@gmail.com'}
-      ]
-    };
-  }
-    render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-        data={this.state.feed}
-        renderItem={({item})=> <Pessoa/>}
-        />
-      </View>
-    );
-  } 
-}
-  const styles = StyleSheet.create({
-    container:{
-      flex:1,
-    }
-  });
-
-export default App;
-
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
-class extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      feed: [
+        {id: '1', nome: 'Bruno', idade: 23, email: 'bruno@gmail.com'},
+        {id: '2', nome: 'Pablo', idade: 24, email: 'pablo@gmail.com'},
+        {id: '3', nome: 'Fulano', idade: 39, email: 'fulano@gmail.com'},
+        {id: '4', nome: 'Ciclano', idade: 18, email: 'ciclano@gmail.com'},
+      ],
     };
   }
-
   render() {
     return (
-      <View>
-        <Text> App </Text>
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.feed}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => <Pessoa data={item} />}
+        />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  areaPessoa: {
+    backgroundColor: '#222',
+    height: 200,
+    marginBottom: 15,
+  },
+  textoPessoa: {
+    color: '#FFF',
+    fontSize: 20,
+  },
+});
+export default App;
 
-export default app;
-
+class Pessoa extends Component {
+  render() {
+    return (
+      <View style={styles.areaPessoa}>
+        <Text> {this.props.data.nome}</Text>
+        <Text style={styles.textoPessoa}>Nome: {this.props.data.nome} </Text>
+        <Text style={styles.textoPessoa}>Idade: {this.props.data.idade} </Text>
+        <Text style={styles.textoPessoa}>Email: {this.props.data.email} </Text>
+      </View>
+    );
+  }
+}
